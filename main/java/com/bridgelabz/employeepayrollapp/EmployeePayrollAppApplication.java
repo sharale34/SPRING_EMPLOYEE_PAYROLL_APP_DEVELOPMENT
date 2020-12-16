c3990ae5f342bc7b35437cb976442e9a840109c8
sharale34 <sourabhharale8000@gmail.com>
2020-12-16 20:03:22 +0530

    Added: Database setting as Environment variable

--- main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
+++ main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -13,5 +13,6 @@ public class EmployeePayrollAppApplication {
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(EmployeePayrollAppApplication.class, args);
 		log.info("Employee Payroll App Started in {} Environment", context.getEnvironment().getProperty("environment"));
+		log.info("Employee Payroll DB User is {}", context.getEnvironment().getProperty("spring.datasource.username"));
 	}
 }
