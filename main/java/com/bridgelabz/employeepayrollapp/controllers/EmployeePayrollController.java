a345ce1cae959e6e7ae0acfd67d1d6c1fcb1ed5b
sharale34 <sourabhharale8000@gmail.com>
2020-12-16 20:51:20 +0530

    Added: Ability to add validation to Name field so that REST call can be validated

--- main/java/com/bridgelabz/employeepayrollapp/controllers/EmployeePayrollController.java
+++ main/java/com/bridgelabz/employeepayrollapp/controllers/EmployeePayrollController.java
@@ -2,6 +2,8 @@ package com.bridgelabz.employeepayrollapp.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -43,7 +45,7 @@ public class EmployeePayrollController {
 	}
 
 	@PostMapping("/create")
-	public ResponseEntity<ResponseDTO> addEmployeePayrollData(@RequestBody EmployeePayrollDTO employeeDTO) {
+	public ResponseEntity<ResponseDTO> addEmployeePayrollData(@Valid @RequestBody EmployeePayrollDTO employeeDTO) {
 		EmployeePayrollData empData = null;
 		empData = employeePayrollService.createEmployeePayrollData(employeeDTO);
 		ResponseDTO respDTO = new ResponseDTO("Get call for ID successful ", empData);
@@ -51,9 +53,10 @@ public class EmployeePayrollController {
 	}
 
 	@PutMapping("/update/{empId}")
-	public ResponseEntity<ResponseDTO> updateEmployeePayrollData(@PathVariable("empId") int empId, @RequestBody EmployeePayrollDTO employeeDTO) {
+	public ResponseEntity<ResponseDTO> updateEmployeePayrollData(@PathVariable("empId") int empId,
+			@Valid @RequestBody EmployeePayrollDTO employeeDTO) {
 		EmployeePayrollData empData = null;
-		empData = employeePayrollService.updateEmployeePayrollData(empId, employeeDTO);		
+		empData = employeePayrollService.updateEmployeePayrollData(empId, employeeDTO);
 		ResponseDTO respDTO = new ResponseDTO("Updated Employee Payroll Data Successfully ", empData);
 		return new ResponseEntity<ResponseDTO>(respDTO, HttpStatus.OK);
 	}
